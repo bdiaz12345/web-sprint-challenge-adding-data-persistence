a423c561f4782587bfa7e8d80f24a226eadda155
bdiaz12345 <bdiazmusic5@gmail.com>
2021-02-19 09:13:42 -0500

    asjdlfkejle;akjf

--- api/task/model.js
+++ api/task/model.js
@@ -7,9 +7,9 @@ module.exports = {
 }
 
 function find() {
-    return db('tasks').where({ task_completed: true || false || 0 || 1 })
+    return db('tasks').where('task_completed', true || false || 0 || 1)
 }
 
 function add(task) {
-    return db('tasks').insert(task).where({ task_completed: true || false || 0 || 1})
+    return db('tasks').insert(task).where('task_completed', true || false || 0 || 1)
 }
