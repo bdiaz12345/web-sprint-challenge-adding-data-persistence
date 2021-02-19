49fda82920a1598e71cf1dd8c25f23d23335d86e
bdiaz12345 <bdiazmusic5@gmail.com>
2021-02-19 07:55:18 -0500

    mvp take 1

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,30 @@
 // build your `/api/projects` router here
+const express = require('express')
+
+const Projects = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.find()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({message: `Server error: ${err}`})
+        })
+})
+
+router.post('/', (req, res) => {
+    const projectData = req.body
+
+    Projects.add(projectData)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({message: `Error: ${err}`})
+        })
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,30 @@
 // build your `/api/tasks` router here
+const express = require('express')
+
+const Tasks = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Tasks.find()
+        .then(tasks => {
+            res.json(tasks)
+        })
+        .catch(err => {
+            res.status(500).json({message: `Server error: ${err}`})
+        })
+})
+
+router.post('/', (req, res) => {
+    const taskData = req.body
+
+    Tasks.add(taskData)
+        .then(task => {
+            res.status(201).json(task)
+        })
+        .catch(err => {
+            res.status(500).json({message: `Error: ${err}`})
+        })
+})
+
+module.exports = router
